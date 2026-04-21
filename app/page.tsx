2b03c4e605f835c08a837ad690c542a8889e4054
Christina Kiesz <ckiesz@gmail.com>
2026-04-20 17:57:21 -0700

    stub pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
  		<div> 
-			<main className="flex flex-col items-center">
+			<main className="flex flex-col items-center m-32 text-4xl">
 			Bakeshop App
 			</main>   
     </div>
---
+++ app/shopid/page.tsx
@@ -0,0 +1,11 @@
+// app/[shopid]/page
+export default function Page() {
+  return (
+ 		<div> 
+			<main className="flex flex-col items-center m-32 ">
+			 <div className="text-4xl">Business Specific Landing Page</div>
+			 <div>This will become the dashboard page for everything at a glance</div>
+			</main>   
+    </div>
+  );
+}
