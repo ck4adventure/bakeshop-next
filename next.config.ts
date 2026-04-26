068db697207689e01f77089caa9a3ad9c966c713
Christina Kiesz <ckiesz@gmail.com>
2026-04-25 20:06:10 -0700

    use workaround plugin

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
+  },
+};
 
 export default nextConfig;
